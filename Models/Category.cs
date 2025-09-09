049c5999aa999e4ba3e03923e86ca7f4b6b69dbb
levent-bal26 <leventbal26@hotmail.com>
2025-09-09 22:21:33 +0300

    Güncelleme mesajı

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+namespace FormsApp.Models
+{
+
+    public class Category
+    {
+
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+
+}
